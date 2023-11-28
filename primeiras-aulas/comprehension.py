424183feb2adbec055f381730903aa8cb6257f27
Anderson Alexsandro Soares <anderson.silva.04@outlook.com>
2023-11-28 15:37:47 -0300

    algumas anotações

--- primeiras-aulas/comprehension.py
+++ primeiras-aulas/comprehension.py
@@ -1,6 +1,6 @@
 l1 = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 l2 = [variavel * 2 for variavel in l1]
-l3 = [(v1 + v2) for v1 in l1 for v2 in range(3)]
+l3 = [(v1 , v2) for v1 in l1 for v2 in range(3)]
 print(l2)
 print(l3)
 
@@ -11,3 +11,12 @@ lista = [
 
 d1 = {x: y for x, y in lista}
 print(d1)
+
+carrinho = []
+carrinho.append(("Produto 1", 30))
+carrinho.append(("Produto 2", 20))
+carrinho.append(("Produto 3", 50))
+
+soma = 0
+soma = sum([float(y) for x, y in carrinho])
+print(soma)
---
+++ primeiras-aulas/geradores.py
@@ -0,0 +1,11 @@
+import time
+
+def gera():
+    for n in range(100):
+        yield n
+        time.sleep(0.1)
+
+g = gera()
+
+for v in g:
+    print(v)
