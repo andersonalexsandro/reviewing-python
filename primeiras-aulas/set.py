c2b160b7fb33707871efce3d7173f4a184b21bbd
Anderson Alexsandro Soares <anderson.silva.04@outlook.com>
2023-11-28 14:24:21 -0300

    sets

---
+++ primeiras-aulas/set.py
@@ -0,0 +1,13 @@
+# add (adiciona), update (atualiza), clear, discard
+# union | (une)
+# intersection & (elementos em comum entre dois sets)
+# difference - (elementros apenas do set da esquerda/primeiro set)
+# symmetric_difference ^ (elementos que estão cada um nos dois sets, mas não em ambos)
+
+s1 = {1, 2, 3, 4, 5, 6, 7, 8, 9}
+
+s1 = set()
+s1.add(1)
+s1.add(1)
+s1.update("Python")
+print(s1)
