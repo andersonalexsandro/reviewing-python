66a055ead326b1afa6e351f475b70afa07b5eeb0
Anderson Alexsandro Soares <anderson.silva.04@outlook.com>
2023-11-29 00:09:02 -0300

    variáveis e métodos estaticos

--- python-OO/ex1-OO/main.py
+++ python-OO/ex1-OO/main.py
@@ -3,3 +3,5 @@ from pessoa import Pessoa
 pessoa1 = Pessoa("Anderson", 19)
 print(pessoa1.nome)
 pessoa1.falar()
+pessoa2 = Pessoa.por_ano_nascimento("Beatriz", 2003)
+print(pessoa2.idade)
--- python-OO/ex1-OO/pessoa.py
+++ python-OO/ex1-OO/pessoa.py
@@ -1,5 +1,8 @@
 class Pessoa:
 
+    #variável estática
+    ano_atual = 2023
+
     def __init__(self, nome, idade, comendo=False, falando=False):
         self.nome = nome
         self.idade = idade
@@ -8,3 +11,8 @@ class Pessoa:
 
     def falar(self):
         print( self.nome, "está falando...")
+
+    @classmethod
+    def por_ano_nascimento(cls, nome, ano_nascimento):
+        idade = cls.ano_atual - ano_nascimento
+        return cls(nome, idade)
